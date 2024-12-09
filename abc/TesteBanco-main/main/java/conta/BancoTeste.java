954fdf8e168f926c6bde2e46fe33910b8915ae59
dragaod <ryanhonoratobsouza@gmail.com>
2024-12-09 13:32:32 -0300

    BancoTeste-Oficial

---
+++ abc/TesteBanco-main/main/java/conta/BancoTeste.java
@@ -0,0 +1,12 @@
+package main.java.conta;
+
+public class BancoTeste{
+
+    public static void main(String[] args) {
+
+                Conta numero_conta = new Conta ("Ryan", 20000);
+
+                numero_conta.Iniciar();
+
+    }
+}
